bea69141246e9c39f534757a7e86c3af5a7e796e
Seyma Helin Kaya <seymahelinkaya@hotmail.com>
2023-09-22 09:45:01 +0300

    argemerkezi

--- argemerkezi/takipsistemi/models.py
+++ argemerkezi/takipsistemi/models.py
@@ -84,8 +84,10 @@ class Message(models.Model):
 from django.db import models
 import uuid
 
+import uuid
+from django.db import models
+
 class Etkinlik(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=200)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
@@ -93,6 +95,8 @@ class Etkinlik(models.Model):
 
     def __str__(self):
         return self.title
+
+
 
 
 class Proje(models.Model):
